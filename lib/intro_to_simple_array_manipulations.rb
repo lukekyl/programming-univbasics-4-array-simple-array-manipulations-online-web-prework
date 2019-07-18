6ecb4d3a4b6c42275b1d9a8f2b90934f8f28f488
Kyle Luke <th.oldschool@gmail.com>
2019-07-18 04:05:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -45,3 +45,27 @@ def using_insert(array, element)
   array.insert(4, element)
   
 end 
+
+def using_uniq(array)
+  
+  array.uniq 
+  
+end
+
+def using_flatten(array)
+  
+  array.flatten
+  
+end
+
+def using_delete(array, element)
+  
+  array.delete element
+  
+end
+
+def using_delete_at(array, integer)
+  
+  array.delete integer
+  
+end
